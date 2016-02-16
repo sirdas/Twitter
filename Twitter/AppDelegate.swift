a45da4b9ff71755468258a6296f18b6a3171d8fa
Sevinc Sirdas <talhasirdas@gmail.com>
2016-02-16 00:25:14 -0600

    a

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -43,27 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        TwitterClient.sharedInstance.fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential(queryString: url.query), success: { (accessToken: BDBOAuth1Credential!) -> Void in
-            print("Got the access token")
-            TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
-            TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
-                print("user: \(response!)")
-                
-                }, failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
-                    print("Error getting current user")
-            })
-            
-            TwitterClient.sharedInstance.GET("1.1/statuses/home_timeline.json", parameters: nil, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
-                print("home timeline: \(response!)")
-                
-                }, failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
-                    print("Error getting home timeline")
-                    
-                })
-            
-            }) { (error: NSError!) -> Void in
-                print("Failed to receive access token")
-        }
+        TwitterClient.sharedInstance.openURL(url)
         return true
     }
 
