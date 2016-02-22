57ea5f9febb05d3fa7062ede08d953ec15f22d8e
Sevinc Sirdas <talhasirdas@gmail.com>
2016-02-21 23:50:19 -0600

    2

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -13,19 +13,23 @@ class Tweet: NSObject {
     var text: String?
     var createdAtString: String?
     var createdAt: NSDate?
+    var formattedDate: String?
+    var retweetCount: Int!
+    var likeCount: Int!
+    var id: String?
     
     init(dictionary: NSDictionary) {
         user = User(dictionary: dictionary["user"] as! NSDictionary)
         text = dictionary["text"] as? String
         createdAtString = dictionary["created_at"] as? String
-        
+        id = String(dictionary["id"]!)
         let formatter = NSDateFormatter()
         formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
         createdAt = formatter.dateFromString(createdAtString!)
-//        formattedDate = formatter.stringFromDate(createdAt!)
-//        retweetCount = dictionary["retweet_count"] as? Int
-//        likeCount = dictionary["favorite_count"] as? Int
-//        id = String(dictionary["id"]!)
+        formattedDate = formatter.stringFromDate(createdAt!)
+        retweetCount = dictionary["retweet_count"] as? Int
+        likeCount = dictionary["favorite_count"] as? Int
+        
     }
     
     class func tweetsWithArray(array: [NSDictionary]) -> [Tweet] {
